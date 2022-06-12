a611420869bdd9b8ad6fd18305cdee01b7d55dcc
okeydokey <okeydokey@gmail.com>
2022-06-12 23:46:17 +0600

    first commit

---
+++ input/app.js
@@ -0,0 +1,105 @@
+const btn = document.querySelector(".btn");
+const lol = document.querySelector(".lol");
+const over = document.querySelector(".over");
+let downloadedData=JSON.parse(localStorage.getItem("noteData"));
+
+let note = `
+<div class="item bg-gray-800 text-white border border-gray-400 rounded-xl px-4 py-6 text-sm flex flex-col items-start gap-y-4">
+  <div class="dlt"><i class="fa-solid fa-delete-left"></i></div>
+            <div class="title">
+                <input type="text" placeholder="Add Title" class="bg-transparent outline-none border-none  text-sm font-bold focus:outline-none mytitle">
+            </div>
+            <div class="notes">
+                <textarea class="text-white mynote text-sm resize-none bg-transparent border-none overflow-hidden  focus:border-none" placeholder="Add Note" name=""  ></textarea>
+            </div>
+        </div>
+  `;
+btn.addEventListener("click", () => {
+ lol.innerHTML += note;
+ ////master array
+ let masterData = JSON.parse(localStorage.getItem("noteData")) || [];
+ let individualData = {
+    title: '',
+    noting: '',
+   };
+masterData.push(individualData);
+localStorage.setItem("noteData", JSON.stringify(masterData));
+thisISGold();
+
+});
+
+function thisISGold() {
+ const items = document.querySelectorAll(".item");
+ const mytitle = document.querySelectorAll(".mytitle");
+ const mynote = document.querySelectorAll(".mynote");
+ const dlt = document.querySelectorAll(".dlt");
+
+ dlt.forEach((element, ind) => {
+  element.onclick = (e) => {
+   e.stopPropagation();
+   downloadedData.splice(ind, 1);
+    localStorage.setItem("noteData", JSON.stringify(downloadedData));
+   items[ind].remove();
+  };
+ });
+ mykostum(items, mytitle, mynote);
+ return items;
+}
+function mykostum(items, mytitle, mynote) {
+ items.forEach((item, ind) => {
+  item.onclick = () => {
+   item.classList.add("klicked");
+   over.classList.add("overlay");
+  };
+  let iscursorinside = false;
+  item.addEventListener("mouseover", () => {
+   iscursorinside = true;
+  });
+  item.addEventListener("mouseout", () => {
+   iscursorinside = false;
+  });
+  document.addEventListener("click", function () {
+   if (!iscursorinside) {
+    if (item.classList.contains("klicked")) {
+     if (mytitle[ind].value != "" && mynote[ind].value != "") {
+        updateData(ind,mytitle,mynote);
+     }
+     over.classList.remove("overlay");
+     item.classList.remove("klicked");
+    }
+   }
+  });
+ });
+}
+window.addEventListener("load", () => {
+let downloadedData=JSON.parse(localStorage.getItem("noteData"));
+
+ for (let x = 0; x < downloadedData.length; x++) {
+
+if(downloadedData[x].title!="" && downloadedData[x].noting!=""){
+    lol.innerHTML += `
+    <div class="item bg-gray-800 text-white border border-gray-400 rounded-xl px-4 py-6 text-sm flex flex-col items-start gap-y-4">
+      <div class="dlt"><i class="fa-solid fa-delete-left"></i></div>
+                <div class="title">
+                    <input type="text" placeholder="Add Title" class="bg-transparent outline-none border-none  text-sm font-bold focus:outline-none mytitle" value="${downloadedData[x].title}">
+                </div>
+                <div class="notes">
+                    <textarea class="text-white mynote text-sm resize-none bg-transparent border-none overflow-hidden focus:border-none" placeholder="Add Note">${downloadedData[x].noting}</textarea>
+                </div>
+            </div>
+      `;
+}
+
+ }
+ const items = document.querySelectorAll(".item");
+ const mytitle = document.querySelectorAll(".mytitle");
+ const mynote = document.querySelectorAll(".mynote");
+ const dlt = document.querySelectorAll(".dlt");
+ thisISGold();
+});
+
+function updateData(ind,mytitle,mynote) {
+    downloadedData[ind].title=mytitle[ind].value;
+    downloadedData[ind].noting=mynote[ind].value;
+    localStorage.setItem("noteData", JSON.stringify(downloadedData));
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./input/**/*.{html,js}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
